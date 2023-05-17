4ac5bc174150043395b754473953c133bd5174ba
Erno Hentonen <erno.hentonen@opiframe.com>
2023-05-17 11:13:53 +0300

    closures

--- javascript/08_closures/public/script.js
+++ javascript/08_closures/public/script.js
@@ -30,7 +30,7 @@ const makeCounter = function() {
 	let privateCounter = 0;
 	
 	function changeBy(val) {
-		privateCounter += val
+		privateCounter += val;
 	}
 	
 	return {
@@ -44,4 +44,26 @@ const makeCounter = function() {
 			return privateCounter;
 		}
 	}
+}
+
+function start() {
+	let counter1 = makeCounter();
+	let counter2 = makeCounter();
+	
+	console.log("Counter 1 value:",counter1.value());
+	console.log("Counter 2 value:",counter2.value());
+	
+	counter1.increment();
+	counter1.increment();
+	counter1.increment();
+	
+	console.log("Counter 1 value:",counter1.value());
+	console.log("Counter 2 value:",counter2.value());
+
+	counter2.decrement();
+	counter2.decrement();
+	counter2.decrement();
+	
+	console.log("Counter 1 value:",counter1.value());
+	console.log("Counter 2 value:",counter2.value());
 }
