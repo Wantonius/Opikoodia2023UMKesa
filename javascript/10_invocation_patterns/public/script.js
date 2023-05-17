bc2bc2ab94877e3834fe943c25a77d7f9ca9de5d
Erno Hentonen <erno.hentonen@opiframe.com>
2023-05-17 13:17:44 +0300

    invocation patterns

--- javascript/10_invocation_patterns/public/script.js
+++ javascript/10_invocation_patterns/public/script.js
@@ -32,4 +32,29 @@ function start() {
 	}	
 	
 	person.calculateAge(10);
+	
+	console.log("------ Constructor invocation pattern ------");
+	
+	var Person2 = function(name) {
+		this.name = name;
+	}
+	
+	Person2.prototype.greet = function() {
+		return this.name+" says hi!";
+	}
+	
+	console.log(new Person2("Calvin").greet());
+	
+	console.log("------ Apply invocation pattern ------");
+	
+	Person2.prototype.waveTo = function(who) {
+		return this.name+" waves to "+who.name;
+	}
+	
+	let calvin = new Person2("Calvin");
+	let hobbes = new Person2("Hobbes");
+	let rover = Object.create({"name":"Rover"});
+	
+	console.log(calvin.waveTo.apply(hobbes,[calvin]));
+	console.log(calvin.waveTo.apply(rover,[hobbes]));
 }
