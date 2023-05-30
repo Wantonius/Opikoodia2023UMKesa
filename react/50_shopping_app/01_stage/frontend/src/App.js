4ecc9203fccd3e04fadf7b42c46d3f07c6b4be85
Erno Hentonen <erno.hentonen@opiframe.com>
2023-05-30 13:20:31 +0300

    shopping app frontend 1

---
+++ react/50_shopping_app/01_stage/frontend/src/App.js
@@ -0,0 +1,113 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState,useEffect} from 'react';
+function App() {
+	
+	const [state,setState] = useState({
+		list:[]
+	})
+	
+	const [urlRequest,setUrlRequest] = useState({
+		url:"",
+		request:{},
+		action:""
+	})
+	
+	//USEEFFECT
+	
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			if(!urlRequest.url) {
+				return;
+			}
+			const response = await fetch(urlRequest.url,urlRequest.request);
+			if(!response) {
+				console.log("No response.");
+				return;
+			}
+			if(response.ok) {
+				switch(urlRequest.action) {
+					case "getlist":
+						const data = await response.json();
+						if(!data) {
+							return;
+						}
+						setState({
+							list:data
+						})
+						return;
+					case "additem":
+					case "removeitem":
+					case "edititem":
+						getList();
+						return;
+					default:
+						return;
+				}
+			} else {
+				console.log("Server responded with a status "+response.status+" "+response.statusText);
+			}
+		}
+		
+		fetchData();
+	},[urlRequest]);
+	
+	//REST API
+	
+	const getList = () => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				"method":"GET"
+			},
+			action:"getlist"			
+		})
+	}
+	
+	const addItem = (item) => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				"method":"POST",
+				"headers":{
+					"Content-Type":"application/json"
+				},
+				"body":JSON.stringify(item)
+			},
+			action:"additem"
+		})
+	}
+	
+	const removeItem = (id) => {
+		setUrlRequest({
+			url:"/api/shopping/"+id,
+			request:{
+				"method":"DELETE"
+			},
+			action:"removeitem"
+		})
+	}
+	
+	const editItem = (item) => {
+		setUrlRequest({
+			url:"/api/shopping/"+item._id,
+			request:{
+				"method":"PUT",
+				"headers":{
+					"Content-Type":"application/json"
+				},
+				"body":JSON.stringify(item)
+			},
+			action:"edititem"
+		})
+	}
+	
+	return (
+		<div className="App">
+
+		</div>
+	);
+}
+
+export default App;
