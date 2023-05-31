87007219170a5ee47b7dedfa4af37369e6fe1c5f
Erno Hentonen <erno.hentonen@opiframe.com>
2023-05-31 10:03:12 +0300

    shopping list stage 01

--- react/50_shopping_app/01_stage/frontend/src/components/ShoppingList.js
+++ react/50_shopping_app/01_stage/frontend/src/components/ShoppingList.js
@@ -1,10 +1,54 @@
 import {useState} from 'react';
 import Row from './Row';
+import RemoveRow from './RemoveRow';
+
 const ShoppingList = (props) => {
+	
+	const [state,setState] = useState({
+		removeIndex:-1,
+		editIndex:-1
+	})
+	
+	const changeMode = (mode,index) => {
+		if(mode === "remove") {
+			setState({
+				removeIndex:index,
+				editIndex:-1
+			})
+		}
+		if(mode === "edit") {
+			setState({
+				removeIndex:-1,
+				editIndex:index
+			})
+		}
+		if(mode === "cancel") {
+			setState({
+				removeIndex:-1,
+				editIndex:-1
+			})
+		}
+	}
+	
+	const removeItem = (id) => {
+		props.removeItem(id);
+		changeMode("cancel");
+	}
+	
+	const editItem = (item) => {
+		props.editItem(item);
+		changeMode("cancel");
+	}
 
 	let items = props.list.map((item,index) => {
+		if(index === state.removeIndex) {
+			return(
+				<RemoveRow key={item._id} item={item} changeMode={changeMode} removeItem={removeItem}/>
+			)
+		}
+		
 		return(
-			<Row key={item._id} item={item} index={index}/>
+			<Row key={item._id} item={item} index={index} changeMode={changeMode}/>
 		)
 	})
 	
