704c9a1df6f3eb9b070e8edb57ee10f173bc5879
Erno Hentonen <erno.hentonen@opiframe.com>
2023-06-06 11:21:51 +0300

    shopping redux

--- react/50_shopping_app/03_stage_redux/frontend/src/index.js
+++ react/50_shopping_app/03_stage_redux/frontend/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import loginReducer from './reducers/loginReducer';
+
+const store = createStore(loginReducer,applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>
   </React.StrictMode>
 );
