e54777d8ec7fb65ac669f4f564c8f8803efe9ed4
Erno Hentonen <erno.hentonen@opiframe.com>
2023-06-13 11:01:20 +0300

    statemanager

--- react/50_shopping_app/04_stage_statemanager/frontend/src/hooks/useAction.js
+++ react/50_shopping_app/04_stage_statemanager/frontend/src/hooks/useAction.js
@@ -15,7 +15,99 @@ const useAction = () => {
 	
 	const {token} = useAppState();
 	
-	useEffect(() => {}, [state]);
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			if(!state.url) {
+				return;
+			}
+			dispatch({
+				"type":actionConstants.LOADING
+			})
+			const response = await fetch(state.url,state.request);
+			dispatch({
+				"type":actionConstants.STOP_LOADING
+			})
+			if(!response) {
+				dispatch({
+					"type":actionConstants.LOGOUT_FAILED,
+					"error":"Server never responded. Logging you out."
+				})
+				return;
+			}
+			if(response.ok) {
+				switch(state.action) {
+					case "register":
+						dispatch({
+							"type":actionConstants.REGISTER_SUCCESS
+						})
+						return;
+					case "login":
+						const data = await response.json();
+						if(!data) {
+							dispatch({
+								"type":actionConstants.LOGIN_FAILED,
+								"error":"Failed to parse login information. Try again later."
+							})
+							return;
+						}
+						dispatch({
+							"type":actionConstants.LOGIN_SUCCESS,
+							"token":data.token
+						})
+						return;
+					case "logout":
+						dispatch({
+							"type":actionConstants.LOGOUT_SUCCESS
+						})
+						return;
+					default:
+						return;
+				}
+			} else {
+				if(response.status === 403) {
+					dispatch({
+						"type":actionConstants.LOGOUT_FAILED,
+						"error":"Your session has expired. Logging you out."
+					})
+					return;
+				}
+				let errorMessage = " Server responded with a status "+response.status+" "+response.statusText;
+				switch(state.action) {
+					case "register":
+						if(response.status === 409) {
+							dispatch({
+								"type":actionConstants.REGISTER_FAILED,
+								"error":"Username already in use."
+							})
+							return;
+						}
+						dispatch({
+							"type":actionConstants.REGISTER_FAILED,
+							"error":"Register failed."+errorMessage
+						})
+						return;
+					case "login":
+						dispatch({
+							"type":actionConstants.LOGIN_FAILED,
+							"error":"Login failed."+errorMessage
+						})
+						return;
+					case "logout":
+						dispatch({
+							"type":actionConstants.LOGOUT_FAILED,
+							"error":"Server responded with an error. Logging you out."
+						})
+						return;
+					default:
+						return;
+				}
+			}
+		}
+		
+		fetchData();
+		
+	}, [state]);
 
 	//FUNCTIONS
 	
